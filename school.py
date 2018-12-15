0b72b2718270db466b2c3296ca6ebafac7097675
minubuls <9studio@bk.ru>
2018-12-15 13:11:11 +0300

    '15.12.18'

--- school.py
+++ school.py
@@ -4,22 +4,26 @@ school = [{'school_class': '4a', 'scores': [3,4,4,5,2]},
 		{'school_class': '7a', 'scores': [3,4,4,5,2]}, 
 		{'school_class': '8a', 'scores': [3,5,5,5,5]}, 
 		{'school_class': '9a', 'scores': [3,4,4,5,2]}, 
-		{'school_class': '10a', 'scores': [1,4,4,5,3]}]
+		{'school_class': '10a', 'scores': [1,4,4,5,3]},
+		{'school_class': '11a', 'scores': [5,4,4,5,5]}
+		]
+
+
+school_rating = 0
 
 
 for ratings in school:
 	sum_items = sum(ratings['scores']) 
 	count_items = len(ratings['scores'])
 	name_class = ratings['school_class']
-	print('Средний балл в классе: ' + name_class, sum_items/count_items)
+	a = sum_items/count_items
+	school_rating = a + school_rating
+	print('Средний балл в классе: ' + name_class, a)
 
 
-def school_rating():
-	for ratings in school:
-		sum_items = sum(ratings['scores']) 
-		count_items = len(ratings['scores'])
-		sr = sum_items/count_items
-		print(sum(list(sr)))
+print('Средний балл по всей школе: ' + str(school_rating/len(school)))
 
 
-school_rating()
+
+
+
